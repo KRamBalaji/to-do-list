996b17d88c87d9a3776a297235dd79faa11a03a0
rambalaji <rambalajikoppula1@gmail.com>
2022-09-07 20:14:11 +0530

    first commit

---
+++ to do list/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  to do list
+//
+//  Created by Ram Balaji Koppula on 07/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ to do list/to_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_listApp.swift
+//  to do list
+//
+//  Created by Ram Balaji Koppula on 07/09/22.
+//
+
+import SwiftUI
+
+@main
+struct to_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
