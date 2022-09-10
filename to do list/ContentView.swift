ddd36669c15c066f2a3cf1765d38fabb669c5a1e
rambalaji <rambalajikoppula1@gmail.com>
2022-09-10 20:39:48 +0530

    created text fields to save tasks

--- to do list/ContentView.swift
+++ to do list/ContentView.swift
@@ -7,10 +7,61 @@
 
 import SwiftUI
 
+enum Priority: String, Identifiable, CaseIterable {
+    var id: UUID {
+        return UUID()
+    }
+    
+    case low = "Low"
+    case medium = "Medium"
+    case high = "High"
+}
+
+extension Priority {
+    var title: String {
+        switch self {
+        case .low:
+            return "Low"
+        case .medium:
+            return "Medium"
+        case .high:
+            return "High"
+        }
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var title: String = ""
+    @State private var selectedPriority: Priority = .medium
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView{
+            
+            VStack{
+                
+                TextField("Enter the title",text: $title)
+                    .textFieldStyle(.roundedBorder)
+                Picker("Priority", selection: $selectedPriority) {
+                    ForEach(Priority.allCases){ priority in
+                        Text(priority.title).tag(priority)
+                    }
+                }
+                .pickerStyle(.segmented)
+                Button("Save") {
+                    
+                }
+                .padding(10)
+                .frame(maxWidth: .infinity)
+                .background(Color.blue)
+                .foregroundColor(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10,style: .continuous))
+                Spacer()
+            }
             .padding()
+            .navigationTitle("Tasks")
+            
+        }
     }
 }
 
